f5b143a8b26f7f305df6f1c2f62fe09b056167dd
MarioMartReq <m.martreq@gmail.com>
2019-12-04 08:59:29 +0000

    Modifications to functions. Final implementation

--- collector.go
+++ collector.go
@@ -14,9 +14,7 @@ import (
 var powerConsumption = prometheus.NewGauge(prometheus.GaugeOpts{
 	Name: "power_Consumption", Help: "Displays the power consumption in Watts of an specific VM"})
 
-func init() {
-	prometheus.MustRegister(powerConsumption)
-	
+func getPower(){
 	cmd := exec.Command("/bin/sh","-c","sudo ipmi-sensors -h localhost --no-sensor-type-output --no-header-output --comma-separated-output --sensor-types Current")
 	if runtime.GOOS == "windows" {
 		cmd = exec.Command("tasklist")
@@ -31,27 +29,17 @@ func init() {
 	
 	watts,err := strconv.ParseFloat(strings.Split(string(out), ",")[2],64)
 	powerConsumption.Set(watts)
+}
 
+func init() {
+	prometheus.MustRegister(powerConsumption)
+	getPower()
 }
 
 func self_update(){
 	rand.Seed(time.Now().Unix())
 	for {
-		cmd := exec.Command("/bin/sh","-c","sudo ipmi-sensors -h localhost --no-sensor-type-output --no-header-output --comma-separated-output --sensor-types Current")
-		if runtime.GOOS == "windows" {
-			cmd = exec.Command("tasklist")
-		}
-
-		out, err := cmd.CombinedOutput()
-		if err != nil {
-			log.Fatalf("cmd.Run() failed with %s\n", err)
-		}
-		// Output from the previous ipmi sensor: 52,PSU_Input_Power,204.00,W,'OK'
-		// We are only interested in the watts, so we only store that part. 
-		
-		watts,err := strconv.ParseFloat(strings.Split(string(out), ",")[2],64)
-		powerConsumption.Set(watts)
-
+		getPower()
 		time.Sleep(time.Second)
 	}
 }
